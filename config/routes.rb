4341d69e394738600622d08985d29edc8040b168
Zak Siraj <zaker.siraj@gmail.com>
2014-09-19 18:08:34 -0400

    Seting root_path to Home Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'home_page/home'
+  #get 'home_page/home'
+  root to: 'home_page#home'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
